c18f713d7e4446e7adafc9129f53e90b433feb02
Hanno <nodscher@icloud.com>
2025-06-19 00:44:26 +0200

    add missing files

---
+++ src/frontend/screens/Settings/components/EscapeFlatpakSandbox.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { ToggleSwitch } from 'frontend/components/UI'
+import useSetting from 'frontend/hooks/useSetting'
+import InfoIcon from 'frontend/components/UI/InfoIcon'
+import { isFlatpak } from 'backend/constants/environment'
+
+const EscapeFlatpakSandbox = () => {
+  const { t } = useTranslation()
+  const [escapeFlatpakSandbox, setEscapeFlatpakSandbox] = useSetting(
+    'useGameMode',
+    false
+  )
+
+  if (!isFlatpak) {
+    return <></>
+  }
+
+  return (
+    <div className="toggleRow">
+      <ToggleSwitch
+        htmlId="escapeflatpaksandbox"
+        value={escapeFlatpakSandbox}
+        handleChange={() => setEscapeFlatpakSandbox(!escapeFlatpakSandbox)}
+        title={t('setting.escapeFlatpakSandbox')}
+      />
+
+      <InfoIcon
+        text={t(
+          'help.EscapeFlatpakSandbox',
+          'Enabling this option allows Heroic to escape the Flatpak sandbox, which mainly allows better integration with Steam. Use with caution. You will have to allow Heroic to talk to org.freedesktop.Flatpak on the session bus manually (in Flatseal/ KDE-Systemsettings).'
+        )}
+      />
+    </div>
+  )
+}
+
+export default EscapeFlatpakSandbox
